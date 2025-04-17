f39bb6ea1c50075c5a550b71343c5b9b0b17ccf5
worldofvictory <133643234+worldofvictory@users.noreply.github.com>
2025-04-17 15:24:10 +0200

    CountApp in Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,38 +1,30 @@
-<script setup lang="ts">
-import ComputedUser from './components/ComputedUser.vue'
-import { ref} from 'vue'
-const name = ref('Hello');
-function changeName(newName: string) {
-  name.value = newName
-}
-</script>
- 
 <template>
+  <main>
 <div>
-    <ComputedUser />
-  
-  <div class="background_box">
-    <div class="element">
-      <p>{{ name }}</p>
-      <button @click="changeName('Goodbye')"> change </button>
-    </div>
-    </div>
-  </div>
+  <h4> the corrent count is..</h4>
+ <h1>{{ count }}</h1> 
+ <button @click="count--">-</button>
+ <button  @click="count++">+</button>
+</div>
+</main>
 </template>
 
-<style scoped>
-.element {
-  display: flex;
-  gap: 20px;
-}
 
-.background_box {
-  width: 100%;
+<script setup lang="ts">
+import {ref} from 'vue';
+let count = ref<number>(0);
+</script>
+
+<style scoped>
+main {
+  background-color: #f2f2f2;
+  width: 100vw;
   height: 100vh;
-  background-color: white;
   display: flex;
   align-items: center;
   justify-content: center;
 }
-
+div{
+  text-align: center;
+}
 </style>
---
+++ vue-project/src/components/ComputedUser1.vue
@@ -0,0 +1,38 @@
+<!--<script setup lang="ts">
+import ComputedUser from './ComputedUser.vue'
+import { ref} from 'vue'
+const name = ref('Hello');
+function changeName(newName: string) {
+  name.value = newName
+}
+</script>
+ 
+<template>
+<div>
+    <ComputedUser />
+  
+  <div class="background_box">
+    <div class="element">
+      <p>{{ name }}</p>
+      <button @click="changeName('Goodbye')"> change </button>
+    </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.element {
+  display: flex;
+  gap: 20px;
+}
+
+.background_box {
+  width: 100%;
+  height: 100vh;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>-->
